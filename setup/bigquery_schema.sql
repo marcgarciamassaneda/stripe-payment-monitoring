0328ece6f3eeef0277627eb24bfaf8ca1c31ff6f
Marc Garcia <marcgarciamassaneda@gmail.com>
2026-05-26 11:57:49 +1200

    Fix dataset name to stripe_data_2 in schema

--- setup/bigquery_schema.sql
+++ setup/bigquery_schema.sql
@@ -1,7 +1,7 @@
 -- Replace YOUR_PROJECT with your GCP project ID
 -- Run this once in BigQuery console before activating either n8n workflow
 
-CREATE TABLE IF NOT EXISTS `YOUR_PROJECT_ID.stripe_data.payments` (
+CREATE TABLE IF NOT EXISTS `YOUR_PROJECT_ID.stripe_data_2.payments` (
   payment_id      STRING NOT NULL,
   customer_id     STRING,
   customer_name   STRING,
